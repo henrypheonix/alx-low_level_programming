0dc60aed1f1ce9359e1a777e3459364d938ec646
henrypheonix <henrylordpheonix6@gmail.com>
2022-12-11 01:55:15 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-  int i = 97;
+int i = 97;
 
-  while (i < 123)
-    {
-      if (i != 101 && i != 113)
-	{
-	  putchar(i);
-	}
-      i++;
-    }
-  putchar(10);
+while (i < 123)
+{
+if (i != 101 && i != 113)
+{
+putchar(i);
+}
+i++;
+}
+putchar(10);
 
-  return (0);
+return (0);
 }

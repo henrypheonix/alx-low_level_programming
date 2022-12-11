fb9d4c8bb78e92b9a2374408d2b4f8eb723014ac
henrypheonix <henrylordpheonix6@gmail.com>
2022-12-11 01:58:16 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,24 +10,24 @@
 
 int main(void)
 {
-  int i, j;
+int i, j;
 
-  i = 97;
-  j = 65;
+i = 97;
+j = 65;
 
-  while (i < 123)
-    {
-      putchar(i);
-      i++;
-    }
+while (i < 123)
+{
+putchar(i);
+i++;
+}
 
-  while (j < 91)
-    {
-      putchar(j);
-      j++;
-    }
+while (j < 91)
+{
+putchar(j);
+j++;
+}
 
-  putchar(10);
+putchar(10);
 
-  return (0);
+return (0);
 }

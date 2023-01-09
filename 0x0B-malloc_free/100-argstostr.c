696df5d6020a1307996a44eddb1c0d7937a6487d
henrypheonix <henrylordpheonix6@gmail.com>
2023-01-09 09:26:29 -0800

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,44 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-/**
- * argstostr - concatenate all arguments of your program with newline
- * @ac: argument count
- * @av: double pointer to array of strings passed to main
- * Return: Null if fail, else return pointer to new string
- */
 
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: arguments count
+ * @av: arguments vector
+ *
+ * Return: a pointer to a new string, or NULL if it fails
+ */
 char *argstostr(int ac, char **av)
 {
-char *a, *retp;
-int i, j, total;
+char *str, *s;
+int i, j, k, len = 0;
 
 if (ac == 0 || av == NULL)
 return (NULL);
 
-for (i = 0, total = 0; i < ac; i++)
-{
-for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
-;
-total++;
-}
-total++;
-
-a = malloc(total * sizeof(char));
-if (a == NULL)
-return (NULL);
-
-retp = a;
 for (i = 0; i < ac; i++)
 {
-for (j = 0; av[i][j] != '\0'; j++)
-{
-*a = av[i][j];
-a++;
-}
-*a = '\n';
-a++;
+s = av[i];
+j = 0;
+
+while (s[j++])
+len++;
+len++;
 }
 
-return (retp);
+str = (char *)malloc(sizeof(char) * (len + 1));
+if (str == NULL)
+return (NULL);
+
+for (i = 0, j = 0; i < ac && j < len; i++)
+{
+s = av[i];
+k = 0;
+
+while (s[k])
+{
+str[j] = s[k];
+k++;
+j++;
+}
+str[j++] = '\n';
+}
+str[j] = '\0';
+
+return (str);
 }

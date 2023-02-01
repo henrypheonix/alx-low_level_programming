fbba54f05aca62f153a40d3655449d6e5c0eb3a5
henrypheonix <henrylordpheonix6@gmil.com>
2023-02-01 13:33:59 -0800

    This is cool

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,23 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint2 - frees a listint_t list
- * @head: head of linked list
+ * free_listint2 - function that frees a listint_t list
+ * @head: a double pointer of lists
+ *
  * Return: void
  */
 
 void free_listint2(listint_t **head)
 {
-listint_t *temp;
+listint_t *next;
 
 if (head == NULL)
 return;
-temp = *head;
-while (temp != NULL)
+while (*head != NULL)
 {
-temp = temp->next;
+next = (*head)->next;
 free(*head);
-*head = temp;
+*head = next;
 }
-*head = NULL;
 }

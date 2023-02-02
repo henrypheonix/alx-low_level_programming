a1219371704de324a78316ffdafff6a135e5f44f
henrypheonix <henrylordpheonix6@gmil.com>
2023-02-02 14:13:23 -0800

    This is cool

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-int _putchar(char c);
+unsigned long int _power(unsigned int base, unsigned int pow);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -9,5 +8,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-unsigned long int _power(unsigned int base, unsigned int pow);
-#endif
+int _putchar(char c);
+
+#endif /*MAIN_H*/

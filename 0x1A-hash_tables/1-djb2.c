91310420917f87b86c1787a819ddb705f52fa2f9
henrypheonix <henrylordpheonix6@gmail.com>
2023-04-06 14:14:30 -0700

    commit

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -10,9 +10,9 @@ unsigned long int hash;
 int c;
 
 hash = 5381;
-while ((c = *str++))
-{
+while ((c = *str++)) {
 hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 }
-return (hash);
+
+return hash;
 }

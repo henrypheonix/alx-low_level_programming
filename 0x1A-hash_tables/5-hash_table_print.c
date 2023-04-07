4977e46c10c882343a79518d74cd68b490673f53
henrypheonix <henrylordpheonix6@gmail.com>
2023-04-06 22:22:13 -0700

    commit

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,30 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - prints a hash table
- * @ht: hash table to print
- *
- * Return: void
+ * hash_table_print - Prints a hash table
+ * @ht: The hash table to print
  */
 void hash_table_print(const hash_table_t *ht)
 {
-unsigned long int i;
-hash_node_t *tmp;
-char flag = 0; /* 0 while no data has been printed yet */
+unsigned int i;
+hash_node_t *node;
+int flag = 0;
 
-if (ht == NULL || ht->array == NULL)
+if (!ht)
 return;
+
 printf("{");
 for (i = 0; i < ht->size; i++)
 {
-tmp = ht->array[i];
-while (tmp != NULL)
+if (ht->array[i])
+{
+node = ht->array[i];
+while (node)
 {
 if (flag == 1)
 printf(", ");
-printf("'%s': '%s'", tmp->keytmp->value);
+printf("'%s': '%s'", node->key, node->value);
 flag = 1;
-tmp = tmp->next;
+node = node->next;
+}
 }
 }
 printf("}\n");

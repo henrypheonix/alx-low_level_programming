8047fad80b4cbe633f7cfe229a3f54db8fa3d443
henrypheonix <henrylordpheonix6@gmail.com>
2023-04-06 21:57:30 -0700

    commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,26 +9,26 @@
  */
 hash_node_t *make_hash_node(const char *key, const char *value)
 {
-  hash_node_t *node;
+hash_node_t *node;
 
-  node = malloc(sizeof(hash_node_t));
-  if (node == NULL)
-    return (NULL);
-  node->key = strdup(key);
-  if (node->key == NULL)
-    {
-      free(node);
-      return (NULL);
-    }
-  node->value = strdup(value);
-  if (node->value == NULL)
-    {
-      free(node->key);
-      free(node);
-      return (NULL);
-    }
-  node->next = NULL;
-  return (node);
+node = malloc(sizeof(hash_node_t));
+if (node == NULL)
+return (NULL);
+node->key = strdup(key);
+if (node->key == NULL)
+{
+free(node);
+return (NULL);
+}
+node->value = strdup(value);
+if (node->value == NULL)
+{
+free(node->key);
+free(node);
+return (NULL);
+}
+node->next = NULL;
+return (node);
 }
 
 
